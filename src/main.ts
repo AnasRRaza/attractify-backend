9c45cabc32a8436041b8322be8c38cc7860360c2
Anas Raza <anas@geeksofkolachi.com>
2025-12-09 22:02:32 +0500

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Attractify API')
+    .setDescription('The Attractify API documentation')
+    .setVersion('1.0')
+    .addTag('attractify')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 8000);
 }
-bootstrap();
+void bootstrap();
